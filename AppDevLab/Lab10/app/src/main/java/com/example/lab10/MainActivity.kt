c0f83a28bc10d589a99b277a143e91ea50229aad
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2022-04-25 22:29:14 +0530

    add: open in maps button in Lab10

--- AppDevLab/Lab10/app/src/main/java/com/example/lab10/MainActivity.kt
+++ AppDevLab/Lab10/app/src/main/java/com/example/lab10/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.lab10
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,6 +13,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textToSpeech : TextToSpeech
     private var textRecognized : String = ""
     private lateinit var surfaceView : SurfaceView
+    private lateinit var openInMap: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             arrayOf(android.Manifest.permission.CAMERA), 1
         )
 
-//        textView = findViewById(R.id.textView)
+        openInMap = findViewById(R.id.map_search)
 
         textToSpeech = TextToSpeech(this@MainActivity) {
             if (it == TextToSpeech.SUCCESS) {
@@ -80,6 +83,15 @@ class MainActivity : AppCompatActivity() {
 //        Toast.makeText(this, textView.text, Toast.LENGTH_SHORT).show()
         textToSpeech.speak(textRecognized, TextToSpeech.QUEUE_FLUSH, null, null)
     }
+
+    fun openInMapClick(view : View){
+        val strUri =
+            "http://maps.google.com/maps?q=$textRecognized"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(strUri))
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity")
+        startActivity(intent)
+    }
+
     fun btnOnClick(view:View){
         if (!textRecognizer.isOperational) {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
